41baf102a9298f871655a7e14b33d3b33ecfc51f
Antonio Prates <antonioprates@gmail.com>
2020-09-21 22:30:27 +0800

    Set sweet clean project structure

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-make clean && echo "Gimme some sugar" && ./configure -s && echo "Baking ingredients..." && make -s && echo "Let's see what we got:" && make install && echo "" && sugar -v && cat sugarc.art
+(cd src && make clean && echo "Gimme some sugar" && ./configure -s && echo "Baking ingredients..." && make -s && echo "Let's see what we got:" && make install && echo "" && sugar -v && cat sugarc.art && make clean)
