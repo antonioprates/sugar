a1f6882e15067e212fbd2ac7da733280dffd976a
Antonio Prates <antonioprates@gmail.com>
2021-12-31 15:22:14 +0800

    Improve words example

--- samples/words.c
+++ samples/words.c
@@ -1,9 +1,15 @@
+#if 0
+  /usr/bin/env sugar `basename $0` $@ && exit;
+  // above is a shebang hack, so you can run with: ./words.c
+#endif
+
 #include <sugar.h>
 
 // simple example of word replacement in a string using sugar.h
 
+string improve(string s) { return replaceWord(s, "alright", "wonderful"); }
+
 app({
   string s = "pizza is alright!";
-  string result = replaceWord(s, "alright", "wonderful");
-  println(result);
+  println(improve(s));
 })
